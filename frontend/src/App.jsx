529435d66205a20e639a1c1a012e79c73a2ca4e3
Leonardo Pereira <leopereirasilva86@gmail.com>
2024-05-07 11:05:26 -0300

    Home and EquipmentList pages created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (   
+      <div>
+          <h1>teste</h1>
+      </div>
+  )
+}
+
+export default App
