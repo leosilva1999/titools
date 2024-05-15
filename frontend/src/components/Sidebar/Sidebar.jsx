6ad318490c1a38a29a2a08e5d9f27ea63404d035
Leonardo Pereira <leopereirasilva86@gmail.com>
2024-05-15 15:21:26 -0300

    sidebar navlink icons added

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from "./Sidebar.module.css"
-import {FaTimes}  from "react-icons/fa";
+import {FaTimes, FaBullhorn, FaLaptop}  from "react-icons/fa";
 import { NavLink } from 'react-router-dom'
 
 const Sidebar = ({ active }) => {
@@ -15,8 +15,8 @@ const Sidebar = ({ active }) => {
         <div className={styles.container}>
             <nav>
                 <ul>
-                    <NavLink className={styles.navlink} to="/equipmentlist"><li><span>Controle de equipamentos</span></li></NavLink>
-                    <NavLink className={styles.navlink} to="/"><li><span>Lista de chamados</span></li></NavLink>
+                    <NavLink className={styles.navlink} to="/equipmentlist"><li><FaLaptop /><span>Controle de equipamentos</span></li></NavLink>
+                    <NavLink className={styles.navlink} to="/"><li><FaBullhorn /><span>Atendimentos</span></li></NavLink>
                 </ul>
             </nav>
         </div>
